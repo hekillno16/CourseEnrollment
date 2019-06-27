1e718b59a14d46497dc031ce430643ff9d1cfbec
He, Liu <lhe@emich.edu>
2019-06-27 08:44:51 -0400

    course-service v0.2

--- microservice-course-management/src/test/java/com/rivers/microservicecoursemanagement/MicroserviceCourseManagementApplicationTests.java
+++ microservice-course-management/src/test/java/com/rivers/microservicecoursemanagement/MicroserviceCourseManagementApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MicroserviceCourseManagementApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
